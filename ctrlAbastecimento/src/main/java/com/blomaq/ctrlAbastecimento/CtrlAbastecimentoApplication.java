85e005daa3f6d9336855d09102bc957d7b82ea52
Cleverson <cleverson.silva@msn.com>
2023-08-25 10:47:33 -0400

    .

---
+++ ctrlAbastecimento/src/main/java/com/blomaq/ctrlAbastecimento/CtrlAbastecimentoApplication.java
@@ -0,0 +1,13 @@
+package com.blomaq.ctrlAbastecimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtrlAbastecimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtrlAbastecimentoApplication.class, args);
+	}
+
+}
---
+++ ctrlAbastecimento/src/test/java/com/blomaq/ctrlAbastecimento/CtrlAbastecimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blomaq.ctrlAbastecimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtrlAbastecimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
